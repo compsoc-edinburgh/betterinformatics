5a185d7f9bebd5262f41f5092348ad75c5231c86
Qais Patankar <qaisjp@gmail.com>
2017-03-08 02:34:07 +0000

    remove exam fromNow suffix

--- static/js/exam-script.js
+++ static/js/exam-script.js
@@ -1,4 +1,4 @@
 $(".examTime").each(function() {
-    let fromNow = moment.unix(parseInt($(this).data("time"))).fromNow();
+    let fromNow = moment.unix(parseInt($(this).data("time"))).fromNow(true);
     $(this).text(fromNow);
 });
