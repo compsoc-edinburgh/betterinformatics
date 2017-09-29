ccf2b2a4b3bd430c503d4941b19816b37f46a639
Qais Patankar <qaisjp@gmail.com>
2017-09-29 19:47:02 +0100

    Add docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Set to the name of the Docker image you want to use
+DOCKER_IMAGE_NAME="binf-jekyll"
+
+# Stop on first error
+set -e
+
+if [ -e Dockerfile ]; then
+  # Build a custom Docker image that has custom Jekyll plug-ins installed
+  docker build --tag $DOCKER_IMAGE_NAME --file Dockerfile .
+
+  # Remove dangling images from previous runs
+  docker rmi -f $(docker images --filter "dangling=true" -q) > /dev/null 2>&1 || true
+else
+  # Use an existing Jekyll Docker image
+  DOCKER_IMAGE_NAME=grahamc/jekyll
+fi
+
+echo "***********************************************************"
+echo "  Your site will be available at http://localhost:4000"
+echo "***********************************************************"
+
+# Start Jekyll and watch for changes
+docker run --rm \
+  --volume=$(pwd):/src:Z \
+  --publish 4000:4000 \
+  $DOCKER_IMAGE_NAME \
+  serve --watch --drafts -H 0.0.0.0
+
