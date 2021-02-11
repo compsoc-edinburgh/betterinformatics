ed7cc46091969f10ec45915a00c98fed8b642042
Qais Patankar <qaisjp@gmail.com>
2021-02-11 00:11:09 +0000

    Fix build scripts

--- docker-run.sh
+++ docker-run.sh
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
 
-# Set to the name of the Docker image you want to use
-DOCKER_IMAGE_NAME="binf-jekyll"
-
 # Stop on first error
 set -e
-
-if [ -e Dockerfile ]; then
-  # Build a custom Docker image that has custom Jekyll plug-ins installed
-  docker build --tag $DOCKER_IMAGE_NAME --file Dockerfile .
-
-  # Remove dangling images from previous runs
-  docker rmi -f $(docker images --filter "dangling=true" -q) > /dev/null 2>&1 || true
-else
-  # Use an existing Jekyll Docker image
-  DOCKER_IMAGE_NAME=grahamc/jekyll
-fi
 
 echo "***********************************************************"
 echo "  Your site will be available at http://localhost:4000"
 echo "***********************************************************"
 
 # Start Jekyll and watch for changes
-docker run --rm \
-  --volume="$(pwd)":/src:Z \
-  --publish 4000:4000 \
-  $DOCKER_IMAGE_NAME \
-  jekyll serve --watch --drafts -H 0.0.0.0
+export JEKYLL_VERSION=3.8
 
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  --publish 4000:4000 \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll serve --watch --drafts -H 0.0.0.0
