52f47d9cede55859c5981c3556dd64a399bed668
yellowtides <andi.dicu@protonmail.com>
2021-06-10 19:23:51 +0300

    Update stylesheet, add dummy theme toggling script

---
+++ static/js/theme.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  const getTheme = window.localStorage && window.localStorage.getItem("theme");
+  const isDark = getTheme === "dark";
+
+  if (getTheme !== null) {
+    document.documentElement.classList.toggle("dark-theme", isDark);
+  }
+
+  const themeBtn = document.querySelector("#useful-links");
+
+  themeBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark-theme");
+    window.localStorage &&
+      window.localStorage.setItem(
+        "theme",
+        document.documentElement.classList.contains("dark-theme") ? "dark" : "light",
+      );
+  });
+}
